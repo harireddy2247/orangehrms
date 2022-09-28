8865f34caa77895a6d0319c4a075a7fab3961ed2
harireddy2247 <113570269+harireddy2247@users.noreply.github.com>
2022-09-28 14:57:40 +0530

    Update run.bat

--- Raghava_POM/run.bat
+++ Raghava_POM/run.bat
@@ -1,5 +1,4 @@
-set projectLocation=C:\Users\reddy\eclipse-workspace\Raghava_POM
+
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
-pause
